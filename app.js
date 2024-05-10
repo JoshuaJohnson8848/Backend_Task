e2d49a76c84fbe191c692aafbc75ac5d7079b226
JoshuaJohnson8848 <joshuatjohnson255@gmail.com>
2024-05-10 14:44:07 +0530

    Bug Fix Up

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 app.use(multer().single('image'));
 
 app.use('',authRouter);
+app.use('/user',userRouter);
 app.use('/admin',adminRouter);
 app.use('/userType',userTypeRouter);
 
