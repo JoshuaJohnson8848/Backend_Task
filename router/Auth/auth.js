ac3d1bcf1548a438c8360cff075f6e82f8d99ec8
JoshuaJohnson8848 <joshuatjohnson255@gmail.com>
2024-05-10 15:51:29 +0530

    Added Google Login and Signup

--- router/Auth/auth.js
+++ router/Auth/auth.js
@@ -5,6 +5,10 @@ const authController = require('../../controller/Auth/auth');
 
 router.post('', authController.login);
 
+router.post('/login', authController.googleLogin);
+
 router.post('/signup', authController.signup);
+
+router.post('/googlesignup', authController.googleSignup);
 
 module.exports = router;
