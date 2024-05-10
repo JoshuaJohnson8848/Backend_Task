8e58e07d25fc26e78558e5ec74050d202a683560
JoshuaJohnson8848 <joshuatjohnson255@gmail.com>
2024-05-10 10:20:13 +0530

    Added update-usertype endpoint

--- router/UserType/usertype.js
+++ router/UserType/usertype.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const isAdmin= require('../../middleware/isAdmin');
+const isAdmin = require('../../middleware/isAdmin');
 
 const userTypeController = require('../../controller/UserType/userType');
 
-router.get('', isAdmin, userTypeController.getAllTypes); 
+router.get('', isAdmin, userTypeController.getAllTypes);
 
 router.post('', isAdmin, userTypeController.addUserType);
+
+router.put('/:id', isAdmin, userTypeController.updateUserType);
 
 router.delete('/:id', isAdmin, userTypeController.deleteUserType);
 
